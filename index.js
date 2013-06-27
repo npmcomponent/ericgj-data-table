199b9e91f7f419e18a3f9e2da2d3b38a076dcc28
Eric Gjertsen <ericgj72@gmail.com>
2013-06-27 00:54:02 -0400

    Add support for reactive views in header and records Update test/index
    Remove model 'update' event subscription which was not really
    fleshed out anyway.

--- index.js
+++ index.js
@@ -9,21 +9,23 @@ function DataTable(el,model){
   this.recordsEl = this.el;
   this.headerEl = this.el;
   this.model = model;
-  if (model && model.on) model.on('update', this.render.bind(this));
+  this._recordViews = [];
   return this;
 }
 
 Emitter(DataTable.prototype);
 
-DataTable.prototype.record = function(tmpl, el){
+DataTable.prototype.record = function(tmpl, viewClass, el){
   this._record = tmpl;
+  this._recordViewClass = viewClass;
   if (el) 
     this.recordsEl = (typeof el == 'string' ? this.el.querySelector(el) : el);
   return this;
 }
 
-DataTable.prototype.header = function(tmpl, el){
+DataTable.prototype.header = function(tmpl, viewClass, el){
   this._header = tmpl;
+  this._headerViewClass = viewClass;
   if (el) 
     this.headerEl = (typeof el == 'string' ? this.el.querySelector(el) : el);
   return this;
@@ -37,11 +39,13 @@ DataTable.prototype.clear = function(){
 
 DataTable.prototype.clearHeader = function(){
   empty(this.headerEl);
+  if (this._headerView) delete this._headerView;
   return this;
 }
 
 DataTable.prototype.clearRecords = function(){
   empty(this.recordsEl);
+  this._recordViews.splice(0,0);
   return this;
 }
 
@@ -54,15 +58,15 @@ DataTable.prototype.render = function(recs){
   this.clearRecords();
   if (this.headerEmpty() && recs.length) this.renderHeader(recs[0]);
   for (var i=0;i<recs.length;++i){
-    this.recordsEl.appendChild(
-      domify(
-        this._record({
-          model: this.model,
-          index: i,
-          record: recs[i]
-        })
-      )[0]
-    );
+    var el;
+    if (this._recordViewClass) {
+      el = domify(this._record);
+      var view = new this._recordViewClass(el, recs[i]);
+      this._recordViews.push(view);
+    } else { 
+      el = rendered(this._record, recs[i], this.model, i); 
+    }
+    this.recordsEl.appendChild(el);
   }
   this.emit('render', recs.length);
 }
@@ -70,19 +74,31 @@ DataTable.prototype.render = function(recs){
 DataTable.prototype.renderHeader = function(rec){
   if (!this._header) return;
   this.clearHeader();
-  this.headerEl.appendChild(
-    domify(
-      this._header({
-        model: this.model,
-        record: rec
-      })
-    )[0]
-  );
-  this.emit('renderHeader');
+  var el;
+  if (this._headerViewClass) {
+    el = domify(this._header);
+    var view = new this._headerViewClass(el, this.model);
+    this._headerView = view;
+  } else {
+    el = rendered(this._header, rec, this.model);
+  }
+  this.headerEl.appendChild(el);
+  this.emit('render header');
 }
 
 
 // private
+
+
+function rendered(tmpl,model,modelClass,i){
+  return domify(
+    tmpl({ model: modelClass,
+           index: i,
+           record: model
+         })
+  );
+}
+
 
 /* inlined from yields/empty */
 
